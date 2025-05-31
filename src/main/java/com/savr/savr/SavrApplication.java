22352b0c033be44b0882454471c108cc33b82204
wesleypds <wlypereiraa@gmail.com>
2025-05-31 15:46:38 -0300

    first commit

---
+++ src/main/java/com/savr/savr/SavrApplication.java
@@ -0,0 +1,13 @@
+package com.savr.savr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavrApplication.class, args);
+	}
+
+}
